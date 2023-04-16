46ba0fa06c98d5c8c6c5022851e6508b7ecb561a
Guilherme F. Videira <123119430+GuilhermeVideira@users.noreply.github.com>
2023-04-16 20:12:22 -0300

    Exercicio02.

---
+++ Exercicio02/main.ts
@@ -0,0 +1,13 @@
+export { };
+
+let nome: string = 'Programa - Zoológico';
+console.log(nome);
+
+let mamiferos: string[] = ['Cachorro', 'Gato', 'Leão', 'Tigre'];
+console.log(mamiferos);
+
+let repteis: string[] = ['Tartaruga', 'Cobra', 'Jacaré', 'Camaleão'];
+console.log(repteis);
+
+let aves: string[] = ['Galo', 'Gavião', 'Papagaio', 'Pato'];
+console.log(aves);
